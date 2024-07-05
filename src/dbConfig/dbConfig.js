b2ddf2ca4dd68eb71f8be20eb3db2c500e28a1ab
Umang-Shroff <143543280+Umang-Shroff@users.noreply.github.com>
2024-07-05 09:09:20 +0530

    Update dbConfig.js

--- src/dbConfig/dbConfig.js
+++ src/dbConfig/dbConfig.js
@@ -2,7 +2,7 @@ import mongoose from 'mongoose';
 
 export async function connect(){
     try{
-        await mongoose.connect("mongodb+srv://iLambor55:123456789101112@cluster0.hnumv7z.mongodb.net/test?retryWrites=true&w=majority&appName=Cluster0");
+        await mongoose.connect("MONGO_URI");
         const connection = mongoose.connection;
 
         connection.on('connected', ()=>{
