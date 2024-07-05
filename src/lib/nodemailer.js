d72791d21cd6b2e2f3104b3020f977f7c7698c4a
Umang-Shroff <143543280+Umang-Shroff@users.noreply.github.com>
2024-07-05 09:10:37 +0530

    Update nodemailer.js

--- src/lib/nodemailer.js
+++ src/lib/nodemailer.js
@@ -4,13 +4,13 @@ import nodemailer from 'nodemailer';
 export const transporter = nodemailer.createTransport({
     service: 'gmail',
     auth:{
-        user: "soyumang55@gmail.com",
-        pass: "tnch gnkk kfnp lxmb"
+        user: "",
+        pass: ""
     }
 })
 
 export const mailOptions = {
-    from: "soyumang55@gmail.com",
-    to: "soyumang55@gmail.com",
+    from: "",
+    to: "",
     
 }
